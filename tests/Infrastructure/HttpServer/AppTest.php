f7eb7ac98875ca19668bc216fe76409e0e8fb5ce
Ignacio El Kadre <imefisto@gmail.com>
2022-07-03 20:29:55 -0300

    feat: configuring namespace for first test

--- tests/Infrastructure/HttpServer/AppTest.php
+++ tests/Infrastructure/HttpServer/AppTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Imefisto\SwooleSlim\Testing\Infrastructure\HttpServer;
 
+use Imefisto\MQTTServer\Infrastructure\HttpServer\App;
+
 class AppTest extends \PHPUnit\Framework\TestCase
 {
 }
